28daf4c5c61d8c5857ee160a0a1e6914768de7c7
Bruno Guignard <guignardbruno@hotmail.fr>
2023-11-19 22:43:09 +0100

    add first fixtures controller

--- src/Controller/FixturesController.php
+++ src/Controller/FixturesController.php
@@ -2,7 +2,40 @@
 
 namespace App\Controller;
 
-class FixturesController
-{
+use App\Repository\Workout\BlockRepository;
+use App\Repository\Workout\BodyPartRepository;
+use App\Repository\Workout\ImplementRepository;
+use App\Repository\Workout\MovementClusterRepository;
+use App\Repository\Workout\MovementRepository;
+use App\Repository\Workout\MovementTypeRepository;
+use App\Repository\Workout\WorkoutRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
+class FixturesController extends AbstractController
+{
+    public function __construct(
+        public readonly BlockRepository $blockRepository,
+        public readonly BodyPartRepository $bodyPartRepository,
+        public readonly ImplementRepository $implementRepository,
+        public readonly MovementClusterRepository $movementClusterRepository,
+        public readonly MovementRepository $movementRepository,
+        public readonly MovementTypeRepository $movementTypeRepository,
+        public readonly WorkoutRepository $workoutRepository,
+    ) {
+    }
+
+    public function getOneFixtureData(string $slug): Response
+    {
+        return $this->render('fixtures/index.html.twig', [
+            'controller_name' => 'FixturesController',
+        ]);
+    }
+
+    public function __invoke(): Response
+    {
+        return $this->render('fixtures/index.html.twig', [
+            'controller_name' => 'FixturesController',
+        ]);
+    }
 }
