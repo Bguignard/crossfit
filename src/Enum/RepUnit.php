953cff74f96a6f159bb280f3ae2427913f26fee8
Bruno Guignard <guignardbruno@hotmail.fr>
2023-11-27 21:31:59 +0100

    Add rep units

--- src/Enum/RepUnit.php
+++ src/Enum/RepUnit.php
@@ -8,4 +8,13 @@ enum RepUnit: string
     case REPETITION = 'repetition';
     case SECOND = 'second';
     case KILOGRAM = 'kilogram';
+    case POUND = 'pound';
+    case FEET = 'feet';
+    case INCH = 'inch';
+    case CALORIE = 'calorie';
+    case YARD = 'yard';
+    case MILE = 'mile';
+    case KILOMETER = 'kilometer';
+    case MINUTE = 'minute';
+    case HOUR = 'hour';
 }
