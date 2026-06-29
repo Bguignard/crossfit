f951d0aed366a42cdad4e64170b2d3370c406ebe
Bruno Guignard <Bguignard@users.noreply.github.com>
2026-06-29 21:34:44 +0200

    Add PHPUnit profile comparison tooling (#400)

--- tests/PhpUnitJUnitSummaryScriptTest.php
+++ tests/PhpUnitJUnitSummaryScriptTest.php
@@ -4,6 +4,8 @@ namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
 
+use function MonWod\Tooling\PhpUnitJUnitSummary\compareSummaries;
+use function MonWod\Tooling\PhpUnitJUnitSummary\renderTextComparison;
 use function MonWod\Tooling\PhpUnitJUnitSummary\renderTextSummary;
 use function MonWod\Tooling\PhpUnitJUnitSummary\summarizeFile;
 
@@ -46,5 +48,41 @@ XML);
         self::assertCount(2, $summary['slowestTests']);
         self::assertStringContainsString('Slowest classes:', renderTextSummary($summary));
         self::assertStringContainsString('App\Tests\SlowIntegrationTest::testApiWorkflow', renderTextSummary($summary));
+    }
+
+    public function testCompareSummariesReportsLargestClassDeltas(): void
+    {
+        $baselinePath = sys_get_temp_dir().'/monwod-phpunit-junit-baseline-'.bin2hex(random_bytes(4)).'.xml';
+        $currentPath = sys_get_temp_dir().'/monwod-phpunit-junit-current-'.bin2hex(random_bytes(4)).'.xml';
+
+        file_put_contents($baselinePath, <<<'XML'
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuites>
+  <testsuite name="Project Test Suite">
+    <testcase class="App\Tests\WorkoutApiWorkflowTest" name="testWorkoutCatalog" assertions="3" time="20.000" />
+    <testcase class="App\Tests\PrivateUserProfileApiTest" name="testProfile" assertions="2" time="10.000" />
+  </testsuite>
+</testsuites>
+XML);
+        file_put_contents($currentPath, <<<'XML'
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuites>
+  <testsuite name="Project Test Suite">
+    <testcase class="App\Tests\WorkoutApiWorkflowTest" name="testWorkoutCatalog" assertions="3" time="12.500" />
+    <testcase class="App\Tests\PrivateUserProfileApiTest" name="testProfile" assertions="2" time="11.250" />
+    <testcase class="App\Tests\NewFastServiceTest" name="testRule" assertions="1" time="0.250" />
+  </testsuite>
+</testsuites>
+XML);
+
+        $comparison = compareSummaries(summarizeFile($baselinePath), summarizeFile($currentPath), 2);
+
+        self::assertSame(30.0, $comparison['baselineTotals']['time']);
+        self::assertSame(24.0, $comparison['currentTotals']['time']);
+        self::assertSame('App\Tests\WorkoutApiWorkflowTest', $comparison['classDeltas'][0]['class']);
+        self::assertSame(-7.5, $comparison['classDeltas'][0]['delta']);
+        self::assertSame('App\Tests\PrivateUserProfileApiTest', $comparison['classDeltas'][1]['class']);
+        self::assertStringContainsString('Totals: 30.000s -> 24.000s (-6.000s)', renderTextComparison($comparison));
+        self::assertStringContainsString('- -7.500s 20.000s -> 12.500s App\Tests\WorkoutApiWorkflowTest', renderTextComparison($comparison));
     }
 }
